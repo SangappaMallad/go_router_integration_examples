f5ccca73ef68a457b6f95183231a667ce782649b
SangappaMallad <sangappamallad0067@gmail.com>
2024-02-08 11:20:26 +0530

    push to local changes

--- lib/projects/routes/app_route_const.dart
+++
@@ -1,9 +0,0 @@
-class MyAppRoutesConstants{
-
-static const String homeRouteName = 'home';
-static const String profileRouteName = 'profile';
-static const String contactUsRouteName = 'contact_us';
-static const String aboutRouteName = 'about';
-
-
-}
