eb76c7802bb76c1de19628b261380dcd6f4a3854
SangappaMallad <sangappamallad0067@gmail.com>
2024-02-08 10:36:19 +0530

    push to main branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router_integration_examples/projects/routes/app_route_config.dart';
+import 'package:go_router_integration_examples/pages/projects/routes/app_route_config.dart';
 
 void main() {
   runApp(const MainApp());
